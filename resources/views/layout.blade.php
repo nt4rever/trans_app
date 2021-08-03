13756076e0e1504e2a4d457d79bcc100717e1841
nt4rever <levantanald@gmail.com>
2021-08-03 14:23:10 +0700

    add mess chat

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,49 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <!-- SEO Meta Tags -->
     <meta name="description" content="Your description">
     <meta name="author" content="Your name">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->
-	<meta property="og:site_name" content="" /> <!-- website name -->
-	<meta property="og:site" content="" /> <!-- website link -->
-	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
-	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
-	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-	<meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
+    <meta property="og:site_name" content="" /> <!-- website name -->
+    <meta property="og:site" content="" /> <!-- website link -->
+    <meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
+    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+    <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+    <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+    <meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
 
     <!-- Webpage Title -->
     <title>Dịch thuật Minh Tuệ</title>
-    
+
     <!-- Styles -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
-    <link href="{{URL::asset('frontend/css/bootstrap.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('frontend/css/fontawesome-all.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('frontend/css/swiper.css')}}" rel="stylesheet">
-	<link href="{{URL::asset('frontend/css/magnific-popup.css')}}" rel="stylesheet">
-	<link href="{{URL::asset('frontend/css/styles.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('frontend/css/custom.css')}}" rel="stylesheet">
-	<script src="{{ URL::asset('ckeditor/ckeditor.js') }}"></script>
-	<!-- Favicon  -->
-    <link rel="icon" href="{{URL::asset("frontend/images/favicon.png")}}">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+        rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/fontawesome-all.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/swiper.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/styles.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('frontend/css/custom.css') }}" rel="stylesheet">
+    <script src="{{ URL::asset('ckeditor/ckeditor.js') }}"></script>
+    <!-- Favicon  -->
+    <link rel="icon" href="{{ URL::asset('frontend/images/favicon.png') }}">
 </head>
+
 <body data-spy="scroll" data-target=".fixed-top">
-    
+
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg fixed-top navbar-light">
         <div class="container">
-            
+
             <!-- Text Logo - Use this if you don't have a graphic logo -->
             <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Pavo</a> -->
 
             <!-- Image Logo -->
-            <a class="navbar-brand logo-image" href="{{URL::to('/')}}"><img src="{{URL::asset('frontend/custom/nmt-logo.png')}}" alt="alternative"></a> 
+            <a class="navbar-brand logo-image" href="{{ URL::to('/') }}"><img
+                    src="{{ URL::asset('frontend/custom/nmt-logo.png') }}" alt="alternative"></a>
 
             <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
                 <span class="navbar-toggler-icon"></span>
@@ -52,26 +56,40 @@
             <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link page-scroll <?php if(isset($home)) echo "active" ?>" href="{{URL::to('/')}}">Trang chủ <span class="sr-only">(current)</span></a>
+                        <a class="nav-link page-scroll <?php if (isset($home)) {
+    echo 'active';
+} ?>" href="{{ URL::to('/') }}">Trang chủ <span
+                                class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll <?php if(isset($service)) echo "active" ?>" href="{{URL::to('/service')}}">Dịch vụ</a>
+                        <a class="nav-link page-scroll <?php if (isset($service)) {
+    echo 'active';
+} ?>" href="{{ URL::to('/service') }}">Dịch
+                            vụ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll <?php if(isset($price)) echo "active" ?>" href="{{URL::to('/price')}}">Báo giá</a>
+                        <a class="nav-link page-scroll <?php if (isset($price)) {
+    echo 'active';
+} ?>" href="{{ URL::to('/price') }}">Báo giá</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll <?php if(isset($post)) echo "active" ?>" href="{{URL::to('/post')}}">Tin tức</a>
+                        <a class="nav-link page-scroll <?php if (isset($post)) {
+    echo 'active';
+} ?>" href="{{ URL::to('/post') }}">Tin tức</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll <?php if(isset($contact)) echo "active" ?>" href="{{URL::to('/contact')}}">Liên hệ</a>
+                        <a class="nav-link page-scroll <?php if (isset($contact)) {
+    echo 'active';
+} ?>" href="{{ URL::to('/contact') }}">Liên
+                            hệ</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Khác</a>
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Khác</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item page-scroll" href="{{URL::to('/library')}}">Thư viện</a>
+                            <a class="dropdown-item page-scroll" href="{{ URL::to('/library') }}">Thư viện</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item page-scroll" href="{{URL::to('/about')}}">Giới thiệu</a>
+                            <a class="dropdown-item page-scroll" href="{{ URL::to('/about') }}">Giới thiệu</a>
                         </div>
                     </li>
                 </ul>
@@ -89,20 +107,21 @@
     <!-- end of navigation -->
 
     @yield('content')
-    
+
     <!-- Footer -->
     <div class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h4>Liên hệ với chúng tôi khi bạn cần hỗ trợ dịch thuật <a class="purple" href="mailto:email@domain.com">email@domain.com</a></h4>
+                    <h4>Liên hệ với chúng tôi khi bạn cần hỗ trợ dịch thuật <a class="purple"
+                            href="mailto:email@domain.com">email@domain.com</a></h4>
                     <div class="social-container">
                         <span class="fa-stack">
                             <a href="#your-link">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-facebook-f fa-stack-1x"></i>
                             </a>
-                        </span> 
+                        </span>
                         <span class="fa-stack">
                             <a href="#your-link">
                                 <i class="fas fa-circle fa-stack-2x"></i>
@@ -131,7 +150,7 @@
                 </div> <!-- end of col -->
             </div> <!-- end of row -->
         </div> <!-- end of container -->
-    </div> <!-- end of footer -->  
+    </div> <!-- end of footer -->
     <!-- end of footer -->
 
 
@@ -151,17 +170,51 @@
                 </div> <!-- end of col -->
             </div> <!-- enf of row -->
         </div> <!-- end of container -->
-    </div> <!-- end of copyright --> 
+    </div> <!-- end of copyright -->
     <!-- end of copyright -->
-    
-    	
+
+    <!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "409764229515442");
+        chatbox.setAttribute("attribution", "biz_inbox");
+
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'v11.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     <!-- Scripts -->
-    <script src="{{URL::asset('frontend/js/jquery.min.js')}}"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
-    <script src="{{URL::asset('frontend/js/bootstrap.min.js')}}"></script> <!-- Bootstrap framework -->
-    <script src="{{URL::asset('frontend/js/jquery.easing.min.js')}}"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
-    <script src="{{URL::asset('frontend/js/swiper.min.js')}}"></script> <!-- Swiper for image and text sliders -->
-    <script src="{{URL::asset('frontend/js/jquery.magnific-popup.js')}}"></script> <!-- Magnific Popup for lightboxes -->
-    <script src="{{URL::asset('frontend/js/scripts.js')}}"></script> <!-- Custom scripts -->
+    <script src="{{ URL::asset('frontend/js/jquery.min.js') }}"></script>
+    <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="{{ URL::asset('frontend/js/bootstrap.min.js') }}"></script>
+    <!-- Bootstrap framework -->
+    <script src="{{ URL::asset('frontend/js/jquery.easing.min.js') }}"></script>
+    <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="{{ URL::asset('frontend/js/swiper.min.js') }}"></script>
+    <!-- Swiper for image and text sliders -->
+    <script src="{{ URL::asset('frontend/js/jquery.magnific-popup.js') }}"></script>
+    <!-- Magnific Popup for lightboxes -->
+    <script src="{{ URL::asset('frontend/js/scripts.js') }}"></script>
+    <!-- Custom scripts -->
     @stack('custom-scripts')
 </body>
+
 </html>
