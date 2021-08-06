6a532291b9b78c291e098827dacf6419cd50aa24
nt4rever <levantanald@gmail.com>
2021-08-06 07:57:24 +0700

    fix stg 2

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             // 'url' => env('APP_URL').'/storage',
-            'url' => env('APP_URL').'/storage/app/public',
+            'url' => env('APP_URL').'storage/app/public',
             'visibility' => 'public',
         ],
 
